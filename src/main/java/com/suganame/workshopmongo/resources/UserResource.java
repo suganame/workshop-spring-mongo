7032707a1b11f7de0249d09e6b9458d62bde8813
Guilherme Suganame <gsuganame@gmail.com>
2024-02-27 14:35:30 -0300

    feat: create DTO pattern to return users data

--- src/main/java/com/suganame/workshopmongo/resources/UserResource.java
+++ src/main/java/com/suganame/workshopmongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.suganame.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.suganame.workshopmongo.domain.User;
+import com.suganame.workshopmongo.dto.UserDTO;
 import com.suganame.workshopmongo.services.UserService;
 
 @RestController
@@ -19,8 +22,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        List<User> list = service.findAll();        
-        return ResponseEntity.ok().body(list);
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<User> list = service.findAll();
+        List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
