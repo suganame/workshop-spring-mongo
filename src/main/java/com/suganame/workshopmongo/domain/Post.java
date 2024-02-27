e256794485c168b078c47aee087ead43847973d6
Guilherme Suganame <gsuganame@gmail.com>
2024-02-27 20:32:24 -0300

    reafactor: migrate Author data with dto

--- src/main/java/com/suganame/workshopmongo/domain/Post.java
+++ src/main/java/com/suganame/workshopmongo/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.suganame.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable {
     @Id
@@ -13,12 +15,12 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -58,11 +60,11 @@ public class Post implements Serializable {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
