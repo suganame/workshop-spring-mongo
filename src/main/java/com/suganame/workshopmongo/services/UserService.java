a6001a6d4b792388cf067f3f3efa015eb2154714
Guilherme Suganame <gsuganame@gmail.com>
2024-02-26 23:30:13 -0300

    feat: create entity User and resource

---
+++ src/main/java/com/suganame/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.suganame.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.suganame.workshopmongo.domain.User;
+import com.suganame.workshopmongo.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
