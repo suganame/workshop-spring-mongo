062cbcf1bbc6cf4d9a22d03b5dab33cb01286787
Guilherme Suganame <gsuganame@gmail.com>
2024-02-28 10:29:33 -0300

    feat: create post's findById method

---
+++ src/main/java/com/suganame/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.suganame.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.suganame.workshopmongo.domain.Post;
+import com.suganame.workshopmongo.repositories.PostRepository;
+import com.suganame.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> Post = repository.findById(id);
+        return Post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
