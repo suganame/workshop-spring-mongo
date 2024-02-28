2a473d0336d195e9ce5101f4690f41ed3745096d
Guilherme Suganame <gsuganame@gmail.com>
2024-02-28 11:48:33 -0300

    feat: add comments to post

--- src/main/java/com/suganame/workshopmongo/domain/Post.java
+++ src/main/java/com/suganame/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.suganame.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.suganame.workshopmongo.dto.AuthorDTO;
+import com.suganame.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -16,6 +19,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -66,6 +71,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
