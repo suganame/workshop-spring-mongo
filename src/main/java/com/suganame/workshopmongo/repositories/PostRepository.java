3a4df1198302c9faffc4e4ca180c10fb56fa0937
Guilherme Suganame <gsuganame@gmail.com>
2024-02-29 10:50:39 -0300

    feat: create query with multiple args

--- src/main/java/com/suganame/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/suganame/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.suganame.workshopmongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,9 +10,12 @@ import org.springframework.stereotype.Repository;
 import com.suganame.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTitleContainingIgnoreCase(String text);
 
     @Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
     List<Post> searchTitle(String text);
+
+    @Query("{ $and : [ { date: { $gte: ?1} }, { date: { $lte: ?2 } } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
